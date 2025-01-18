1ce6cf5c9d4a9b39a14dd68182ba4a51d0d2f8ef
Casanova-90 <manuel.casanova@gmail.com>
2025-01-18 13:34:19 -0600

    Add-env-var

--- backend/main.go
+++ backend/main.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -16,17 +15,30 @@ import (
 )
 
 func connect() (*sql.DB, error) {
-	bin, err := ioutil.ReadFile("/run/secrets/db-password")
-	if err != nil {
-		return nil, err
+	// Get the environment variables
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	// If any of the environment variables are missing, return an error
+	if dbHost == "" || dbUser == "" || dbPassword == "" || dbPort == "" || dbName == "" {
+		return nil, fmt.Errorf("missing required environment variables")
 	}
-	return sql.Open("postgres", fmt.Sprintf("postgres://postgres:%s@db:5432/example?sslmode=disable", string(bin)))
+
+	// Construct the connection string using the environment variables
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	// Connect to the database
+	return sql.Open("postgres", connStr)
 }
 
 func blogHandler(w http.ResponseWriter, r *http.Request) {
 	db, err := connect()
 	if err != nil {
 		w.WriteHeader(500)
+		fmt.Fprintf(w, "Error connecting to the database: %v", err)
 		return
 	}
 	defer db.Close()
@@ -34,19 +46,28 @@ func blogHandler(w http.ResponseWriter, r *http.Request) {
 	rows, err := db.Query("SELECT title FROM blog")
 	if err != nil {
 		w.WriteHeader(500)
+		fmt.Fprintf(w, "Error querying the database: %v", err)
 		return
 	}
+
 	var titles []string
 	for rows.Next() {
 		var title string
 		err = rows.Scan(&title)
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprintf(w, "Error scanning row: %v", err)
+			return
+		}
 		titles = append(titles, title)
 	}
+
 	json.NewEncoder(w).Encode(titles)
 }
 
 func main() {
 	log.Print("Prepare db...")
+
 	if err := prepare(); err != nil {
 		log.Fatal(err)
 	}
@@ -64,6 +85,7 @@ func prepare() error {
 	}
 	defer db.Close()
 
+	// Retry until the database is available
 	for i := 0; i < 60; i++ {
 		if err := db.Ping(); err == nil {
 			break
@@ -71,6 +93,7 @@ func prepare() error {
 		time.Sleep(time.Second)
 	}
 
+	// Drop the table if it exists and create a new one
 	if _, err := db.Exec("DROP TABLE IF EXISTS blog"); err != nil {
 		return err
 	}
@@ -79,6 +102,7 @@ func prepare() error {
 		return err
 	}
 
+	// Insert some sample blog posts
 	for i := 0; i < 5; i++ {
 		if _, err := db.Exec("INSERT INTO blog (title) VALUES ($1);", fmt.Sprintf("Blog post #%d", i)); err != nil {
 			return err
